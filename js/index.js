fce3419c57df802e5698a2a20088b88711a5e1ee
grindevelop <grindevelop@gmail.com>
2020-11-08 21:26:49 +0300

    bug fix

--- js/index.js
+++ js/index.js
@@ -52,6 +52,7 @@ function getRecordsFromCSVFileAsync(file) {
   return new Promise((resolve, reject) => {
     Papa.parse(file, {
       header: true,
+      skipEmptyLines: true,
       complete: (results) => resolve(results.data),
       error: reject,
     });
