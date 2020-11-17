010ce1d585e871d409144eee5428ac0c21e07cad
grindevelop <grindevelop@gmail.com>
2020-11-17 23:10:57 +0300

    drop txt file format

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-class MessageRecord {
-  /**
-   * 
-   * @param {string | number} timestamp 
-   * @param {string} author 
-   * @param {string} message 
-   */
-  constructor(timestamp, message) {
-    this.timestamp = timestamp;
-    this.message = message;
-  }
-}
-
-/**
- * 
- * @param {File} file 
- * @returns {Promise<string>}
- */
 function readTextFileAsync(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -25,122 +7,78 @@ function readTextFileAsync(file) {
   });
 }
 
-/**
- * 
- * @param {File} file 
- * @returns {Promise<MessageRecord[]>}
- */
-async function getRecordsFromTextFileAsync(file) {
-  const text = await readTextFileAsync(file);
-  const records = [];
-  for (const line of text.split("\n")) {
-    const i = line.indexOf(" ");
-    if (i < 1) {
-      continue;
-    }
-    records.push(new MessageRecord(line.slice(0, i), line.slice(i + 1)));
+function downloadEmotes(list, size) {
+  const map = new Map();
+  for (const line of list.match(/[^\r\n]+/g)) {
+    const fields = line.match(/\S+/g);
+    const name = fields[0];
+    const url = fields[1];
+    const img = new Image(size, size);
+    img.crossOrigin = 'anonymous';
+    img.src = url;
+    map.set(name, img);
   }
-  return records;
+  return new Emotes(map, size);
 }
 
-/**
- * 
- * @param {File} file 
- * @returns {Promise<MessageRecord[]>}
- */
-function getRecordsFromCSVFileAsync(file) {
+function getRecords(csvFile) {
   return new Promise((resolve, reject) => {
     const records = [];
-    Papa.parse(file, {
+    Papa.parse(csvFile, {
       header: true,
       skipEmptyLines: true,
-      step: (results) => {
-        if (results.error) {
-          console.warn(results.error);
-        }
-        const timestamp = results.data.timestamp;
-        const message = results.data.author
-          ? `${results.data.author}: ${results.data.message}`
-          : results.data.message;
-        records.push(new MessageRecord(timestamp, message));
-      },
-      complete: () => resolve(records),
+      complete: (results) => resolve(results.data),
       error: reject,
     });
   });
 }
 
-/**
- * 
- * @param {File} file 
- * @returns {Promise<MessageRecord[]>}
- */
-function getRecordsFromFileAsync(file) {
-  if (file.name.endsWith(".csv")) {
-    return getRecordsFromCSVFileAsync(file);
-  }
-  return getRecordsFromTextFileAsync(file);
-}
-
-/**
- * 
- * @param {MessageRecord[]} records 
- * @param {{table: Map<string, CanvasImageSource>, emojiSize: number}} emojiSet 
- * @param {HTMLCanvasElement} canvas 
- * @param {string} bgFillStyle 
- * @param {string} font 
- * @param {string} fontFillStyle 
- */
-async function render(
-  records,
-  emojiSet,
-  canvas,
-  bgFillStyle,
-  font,
-  fontFillStyle,
-) {
-  const chat = new ChatDrawer(canvas.width, canvas.height, emojiSet);
-  const zip = new JSZip();
-  const ffmpegScript = ["ffconcat version 1.0"];
-  const ctx = canvas.getContext("2d");
+async function render(records, chat, canvas, font) {
+  const ctx = canvas.getContext('2d');
   ctx.font = font;
+  const zip = new JSZip();
+  const ffmpegScript = ['ffconcat version 1.0'];
   records.push(records[records.length - 1]);
   for (let i = 0; i < records.length - 1; i++) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = bgFillStyle;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = fontFillStyle;
-    chat.append(records[i].message, ctx);
+    chat.append(records[i].author, records[i].message, ctx);
     chat.draw(ctx, 0, 0);
     const blob = await new Promise((resolve) => canvas.toBlob(resolve));
     const filepath = `img/${i}.png`;
     zip.file(filepath, blob);
     ffmpegScript.push(`file ${filepath}`);
-    const d = (records[i + 1].timestamp - records[i].timestamp) / 1000000;
-    ffmpegScript.push(`duration ${d}`);
+    const duration = (records[i + 1].timestamp - records[i].timestamp) / 1000000;
+    ffmpegScript.push(`duration ${duration}`);
   }
   records.pop();
-  zip.file("script.txt", ffmpegScript.join("\n"));
-  zip.generateAsync({ type: "blob" }).then((content) =>
-    saveAs(content, "render.zip")
+  zip.file('script.txt', ffmpegScript.join('\n'));
+  zip.generateAsync({ type: 'blob' }).then((content) =>
+    saveAs(content, 'render.zip')
   );
 }
 
 async function main() {
-  const configForm = document.forms.namedItem("config");
+  const configForm = document.forms.config;
+  const width = parseFloat(configForm.width.value);
+  const height = parseFloat(configForm.height.value);
   const bgFillStyle = configForm.bgFillStyle.value;
-  const fontFillStyle = configForm.fontFillStyle.value;
-  const fontFamily = configForm.fontFamily.value;
-  const fontSize = configForm.fontSize.value;
-  const font = `${fontSize}px ${fontFamily}`;
-  const canvas = document.createElement("canvas");
-  canvas.width = configForm.width.value;
-  canvas.height = configForm.height.value;
-  const emojiList = await readTextFileAsync(configForm.emojisFile.files[0]);
-  const emojiSet = downloadEmojiSet(emojiList, fontSize * 1.5);
-  const messagesFile = configForm.messagesFile.files[0];
-  const records = await getRecordsFromFileAsync(messagesFile);
+  const font = configForm.font.value;
+  const emotesSize = parseFloat(configForm.emotesSize.value);
+  const authorFillStyle = configForm.authorFillStyle.value;
+  const messageFillStyle = configForm.messageFillStyle.value;
+  const emotesFile = configForm.emotes.files[0];
+  const messagesFile = configForm.messages.files[0];
+
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  const messageStyle = new MessageStyle(authorFillStyle, messageFillStyle);
+  const chatStyle = new ChatStyle(bgFillStyle, messageStyle);
+  const emotesList = await readTextFileAsync(emotesFile);
+  const emotes = downloadEmotes(emotesList, emotesSize);
+  const chat = new ChatDrawer(width, height, emotes, emotesSize / 2, chatStyle);
+  const records = await getRecords(messagesFile);
   configForm.remove();
   document.body.append(canvas);
-  render(records, emojiSet, canvas, bgFillStyle, font, fontFillStyle);
+  render(records, chat, canvas, font);
 }
