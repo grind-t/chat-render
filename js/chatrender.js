c1d188852e694bd217f9b5257664c545e9473c92
grindevelop <grindevelop@gmail.com>
2020-11-17 23:10:10 +0300

    add author color

--- js/chatrender.js
+++ js/chatrender.js
@@ -1,16 +1,4 @@
-/**
- * @typedef {object} Block
- * @property {Rectangle} rect
- */
-
 class Rectangle {
-  /**
-   * 
-   * @param {number} x 
-   * @param {number} y 
-   * @param {number} width 
-   * @param {number} height 
-   */
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
@@ -19,234 +7,160 @@ class Rectangle {
   }
 }
 
-class FlexRowWrapLayout {
-  /**
-   * 
-   * @param {number} width
-   * @param {number} itemsSpacing 
-   * @param {number} rowHeight 
-   * @param {"bottom" | "center" | "top"} itemsAlign 
-   */
-  constructor(width, itemsSpacing, rowHeight, itemsAlign) {
-    this.width = width;
-    this.itemsSpacing = itemsSpacing;
-    this.rowHeight = rowHeight;
-    this.itemsAlign = itemsAlign;
+class StringDrawer extends Rectangle {
+  constructor(string, width, height) {
+    super(0, 0, width, height);
+    this.string = string;
+  }
+
+  draw(ctx, offsetX, offsetY) {
+    const x = this.x + offsetX;
+    const y = this.y + offsetY + this.height;
+    ctx.fillText(this.string, x, y);
   }
 }
 
-class EmojiSet {
-  /**
-   * 
-   * @param {Map<string, CanvasImageSource} table 
-   * @param {number} emojiSize 
-   */
-  constructor(table, emojiSize) {
-    this.table = table;
-    this.emojiSize = emojiSize;
+class ImageDrawer extends Rectangle {
+  constructor(image, width, height) {
+    super(0, 0, width, height);
+    this.image = image;
+  }
+
+  draw(ctx, offsetX, offsetY) {
+    const x = this.x + offsetX;
+    const y = this.y + offsetY;
+    ctx.drawImage(this.image, x, y, this.width, this.height);
   }
 }
 
-class StringDrawer {
-  /**
-   * 
-   * @param {string} str 
-   * @param {Rectangle} rect 
-   */
-  constructor(str, rect) {
-    this.str = str;
-    this.rect = rect;
-  }
+class Emotes {
+  constructor(map, size) {
+    this.map = map;
+    this.size = size;
+    this.regexp = null;
 
-  /**
-   * 
-   * @param {CanvasRenderingContext2D} ctx 
-   * @param {number} x 
-   * @param {number} y 
-   */
-  draw(ctx, x, y) {
-    ctx.fillText(this.str, x + this.rect.x, y + this.rect.y + this.rect.height);
-  }
-}
-
-class ImageDrawer {
-  /**
-   * 
-   * @param {CanvasImageSource} img 
-   * @param {Rectangle} rect 
-   */
-  constructor(img, rect) {
-    this.img = img;
-    this.rect = rect;
-  }
-
-  /**
-   * 
-   * @param {CanvasRenderingContext2D} ctx 
-   * @param {number} x 
-   * @param {number} y 
-   */
-  draw(ctx, x, y) {
-    ctx.drawImage(
-      this.img,
-      x + this.rect.x,
-      y + this.rect.y,
-      this.rect.width,
-      this.rect.height,
-    );
-  }
-}
-
-class MessageDrawer {
-  /**
-   * 
-   * @param {number} width 
-   * @param {string[]} fields 
-   * @param {EmojiSet} emojiSet 
-   * @param {CanvasRenderingContext2D} ctx 
-   */
-  constructor(width, fields, emojiSet, ctx) {
-    const fontHeight = ctx.measureText("W").actualBoundingBoxAscent;
-    this.blocks = [];
-    this.rect = new Rectangle(0, 0, width, 0);
-    for (const field of fields) {
-      const emoji = emojiSet.table.get(field);
-      if (emoji) {
-        const rect = new Rectangle(0, 0, emoji.width, emoji.height);
-        this.blocks.push(new ImageDrawer(emoji, rect));
-        continue;
-      }
-      const metrics = ctx.measureText(field);
-      if (metrics.width <= width) {
-        const rect = new Rectangle(0, 0, metrics.width, fontHeight);
-        this.blocks.push(new StringDrawer(field, rect));
-        continue;
-      }
-      const strings = splitLongWord(field, width, ctx);
-      for (const s of strings) {
-        const metrics = ctx.measureText(s);
-        const rect = new Rectangle(0, 0, metrics.width, fontHeight);
-        this.blocks.push(new StringDrawer(field, rect));
-      }
+    let pattern = '';
+    for (name of map.keys()) {
+      name.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+      pattern += name + '|';
     }
-    const spacing = ctx.measureText(" ").width;
-    const rowHeight = emojiSet.emojiSize;
-    const layout = new FlexRowWrapLayout(
-      this.rect.width,
-      spacing,
-      rowHeight,
-      "center",
-    );
-    const height = flexRowWrap(this.blocks, layout);
-    this.rect = new Rectangle(0, 0, width, height);
-  }
-
-  /**
-   * 
-   * @param {CanvasRenderingContext2D} ctx 
-   * @param {number} x 
-   * @param {number} y 
-   */
-  draw(ctx, x, y) {
-    this.blocks.forEach((b) => b.draw(ctx, x + this.rect.x, y + this.rect.y));
+    pattern = pattern.slice(0, -1); // Remove last '|'.
+    this.regexp = new RegExp(pattern, 'g');
   }
 }
 
-class ChatDrawer {
-  /**
-   * 
-   * @param {number} width 
-   * @param {number} height 
-   * @param {EmojiSet} emojiSet 
-   */
-  constructor(width, height, emojiSet) {
-    this.rect = new Rectangle(0, 0, width, height);
-    this.messages = [];
-    this._emojiSet = emojiSet;
-    // TODO: escape names?
-    this._emojisRegExp = new RegExp(
-      Array.from(emojiSet.table.keys()).join("|"),
-      "g",
-    );
+class MessageStyle {
+  constructor(authorFillStyle, messageFillStyle) {
+    this.authorFillStyle = authorFillStyle;
+    this.messageFillStyle = messageFillStyle;
+  }
+}
+
+class MessageLayout {
+  constructor(width, spacing, lineHeight, verticalAlign) {
+    this.width = width;
+    this.spacing = spacing;
+    this.lineHeight = lineHeight;
+    this.verticalAlign = verticalAlign;
   }
 
-  /**
-   * 
-   * @param {string} message 
-   * @param {CanvasRenderingContext2D} ctx 
-   */
-  append(message, ctx) {
-    message = message.replace(this._emojisRegExp, (match) => ` ${match} `);
-    this.messages.push(
-      new MessageDrawer(
-        this.rect.width,
-        message.split(/\s+/),
-        this._emojiSet,
-        ctx,
-      ),
-    );
-    const length = this.messages.length;
+  arrange(...items) {
+    let lineY = 0;
+    let lineWidth = 0;
+    for (const item of items) {
+      if (lineWidth + item.width > this.width) {
+        lineWidth = 0;
+        lineY += this.lineHeight;
+      }
+      item.x = lineWidth;
+      item.y = lineY;
+      if (this.verticalAlign === 'center') {
+        item.y += this.lineHeight / 2 - item.height / 2;
+      } else if (this.verticalAlign === 'bottom') {
+        item.y += this.lineHeight - item.height;
+      }
+      lineWidth += item.width + this.spacing;
+    }
+    const totalHeight = lineY + this.lineHeight;
+    return totalHeight;
+  }
+}
+
+class MessageDrawer extends Rectangle {
+  constructor(width, height, authorDrawer, drawers, style) {
+    super(0, 0, width, height);
+    this.authorDrawer = authorDrawer;
+    this.drawers = drawers;
+    this.style = style;
+  }
+
+  draw(ctx, offsetX, offsetY) {
+    const x = this.x + offsetX;
+    const y = this.y + offsetY;
+    ctx.fillStyle = this.style.authorFillStyle;
+    this.authorDrawer.draw(ctx, x, y);
+    ctx.fillStyle = this.style.messageFillStyle;
+    this.drawers.forEach(drawer => drawer.draw(ctx, x, y));
+  }
+}
+
+class ChatStyle {
+  constructor(backgroundFillStyle, messageStyle) {
+    this.backgroundFillStyle = backgroundFillStyle;
+    this.messageStyle = messageStyle;
+  }
+}
+
+class ChatDrawer extends Rectangle {
+  constructor(width, height, emotes, verticalSpacing, style) {
+    super(0, 0, width, height);
+    this.emotes = emotes;
+    this.verticalSpacing = verticalSpacing;
+    this.style = style;
+    this.messageDrawers = [];
+  }
+
+  append(author, message, ctx) {
+    const spacing = ctx.measureText(' ').width;
+    const layout = new MessageLayout(this.width, spacing, this.emotes.size, 'center');
+    const fontHeight = ctx.measureText('W').actualBoundingBoxAscent;
+    const authorWidth = ctx.measureText(author).width;
+    const authorDrawer = new StringDrawer(author, authorWidth, fontHeight);
+    const drawers = getDrawers(message, this.emotes, this.width, ctx);
+    const height = layout.arrange(authorDrawer, ...drawers);
+    const md = new MessageDrawer(this.width, height, authorDrawer, drawers, this.style.messageStyle);
+    this.messageDrawers.push(md);
+    const length = this.messageDrawers.length;
     if (length < 2) {
       return;
     }
-    const prevMsg = this.messages[length - 2];
-    const currMsg = this.messages[length - 1];
-    const vSpacing = this._emojiSet.emojiSize / 2;
-    currMsg.rect.y = prevMsg.rect.y + prevMsg.rect.height + vSpacing;
-    this.messages.splice(0, scrollDown(this.messages, this.rect));
+    const prev = this.messageDrawers[length - 2];
+    const curr = this.messageDrawers[length - 1];
+    curr.y = prev.y + prev.height + this.verticalSpacing;
+    const outOfBounds = scrollDown(this.messageDrawers, this.y, this.y + this.height);
+    this.messageDrawers.splice(0, outOfBounds);
   }
 
-  /**
-   * 
-   * @param {CanvasRenderingContext2D} ctx 
-   * @param {number} x 
-   * @param {number} y 
-   */
-  draw(ctx, x, y) {
-    this.messages.forEach((m) => m.draw(ctx, x + this.rect.x, y + this.rect.y));
+  draw(ctx, offsetX, offsetY) {
+    const x = this.x + offsetX;
+    const y = this.y + offsetY;
+    ctx.fillStyle = this.style.backgroundFillStyle;
+    ctx.fillRect(x, y, this.width, this.height);
+    this.messageDrawers.forEach(drawer => drawer.draw(ctx, x, y));
   }
 }
 
-/**
- * 
- * @param {string} list 
- * @param {number} emojiSize 
- * @returns {EmojiSet}
- */
-function downloadEmojiSet(list, emojiSize) {
-  const table = new Map();
-  for (const line of list.split("\n")) {
-    if (!line) {
-      continue;
-    }
-    const fields = line.split(" ");
-    const name = fields[0];
-    const url = fields[1];
-    const img = new Image(emojiSize, emojiSize);
-    img.crossOrigin = "anonymous";
-    img.src = url;
-    table.set(name, img);
-  }
-  return new EmojiSet(table, emojiSize);
-}
-
-/**
- * 
- * @param {string} word 
- * @param {number} maxwidth 
- * @param {CanvasRenderingContext2D} ctx 
- */
-function splitLongWord(word, maxwidth, ctx) {
+function splitLongWord(word, maxWidth, ctx) {
   const words = [];
   for (let i = 0, end = word.length; i < end; i++) {
     let width = ctx.measureText(word.slice(0, i + 1)).width;
-    if (width <= maxwidth) {
+    if (width <= maxWidth) {
       continue;
     }
     words.push(word.slice(0, i));
     word = word.slice(i);
     width = ctx.measureText(word).width;
-    if (width <= maxwidth) {
+    if (width <= maxWidth) {
       words.push(word);
       break;
     }
@@ -256,49 +170,61 @@ function splitLongWord(word, maxwidth, ctx) {
   return words;
 }
 
-/**
- * 
- * @param {Block[]} blocks
- * @param {FlexRowWrapLayout} layout 
- * @returns {number} height
- */
-function flexRowWrap(blocks, layout) {
-  let totalHeight = 0;
-  let lineWidth = 0;
-  for (let block of blocks) {
-    if (lineWidth + block.rect.width > layout.width) {
-      lineWidth = 0;
-      totalHeight += layout.rowHeight;
-    }
-    block.rect.x = lineWidth;
-    block.rect.y = totalHeight;
-    if (layout.itemsAlign === "center") {
-      block.rect.y += layout.rowHeight / 2 - block.rect.height / 2;
-    } else if (layout.itemsAlign === "bottom") {
-      block.rect.y += layout.rowHeight - block.rect.height;
-    }
-    lineWidth += block.rect.width + layout.itemsSpacing;
+function getStringDrawers(words, maxWidth, ctx) {
+  if (!words) {
+    return null;
   }
-  totalHeight += layout.rowHeight;
-  return totalHeight;
+  const fontHeight = ctx.measureText('W').actualBoundingBoxAscent;
+  const drawers = [];
+  for (const word of words) {
+    const width = ctx.measureText(word).width;
+    if (width <= maxWidth) {
+      drawers.push(new StringDrawer(word, width, fontHeight));
+      continue;
+    }
+    const words = splitLongWord(word, maxWidth, ctx);
+    for (const word of words) {
+      const width = ctx.measureText(word).width;
+      drawers.push(new StringDrawer(word, width, fontHeight));
+    }
+  }
+  return drawers;
 }
 
-/**
- * 
- * @param {Block[]} blocks 
- * @param {Rectangle} bounds
- * @returns {number} out of bounds
- */
-function scrollDown(blocks, bounds) {
-  const last = blocks[blocks.length - 1];
-  const offset = (last.rect.y + last.rect.height) - (bounds.y + bounds.height);
-  if (offset <= 0) {
-    return 0;
+function getDrawers(message, emotes, maxWidth, ctx) {
+  const drawers = [];
+  const matches = message.matchAll(emotes.regexp);
+  let i = 0;
+  for (const match of matches) {
+    const str = message.slice(i, match.index);
+    const words = str.match(/\S+/g);
+    const stringDrawers = getStringDrawers(words, maxWidth, ctx);
+    if (stringDrawers) {
+      drawers.push(...stringDrawers);
+    }
+    const img = emotes.map.get(match[0]);
+    drawers.push(new ImageDrawer(img, emotes.size, emotes.size));
+    i += str.length + match[0].length;
   }
+  const str = message.slice(i, message.length);
+  const words = str.match(/\S+/g);
+  const stringDrawers = getStringDrawers(words, maxWidth, ctx);
+  if (stringDrawers) {
+    drawers.push(...stringDrawers);
+  }
+  return drawers;
+}
+
+function scrollDown(drawers, minY, maxY) {
   let outOfBounds = 0;
-  for (const block of blocks) {
-    block.rect.y -= offset;
-    if (block.rect.y + block.rect.height <= bounds.y) {
+  const last = drawers[drawers.length - 1];
+  const offset = last.y + last.height - maxY;
+  if (offset <= 0) {
+    return outOfBounds;
+  }
+  for (const drawer of drawers) {
+    drawer.y -= offset;
+    if (drawer.y + drawer.height <= minY) {
       outOfBounds++;
     }
   }
--- js/chatrender.test.js
+++ js/chatrender.test.js
@@ -1,36 +1,48 @@
-describe("chatrender", () => {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-  it("splitLongWord", () => {
-    const longword = "w".repeat(15);
+describe('chatrender', () => {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  it('splitLongWord', () => {
+    const longword = 'w'.repeat(15);
     const width = ctx.measureText(longword).width;
     assert.strictEqual(Math.floor(width), 108);
     const maxwidth = 50;
-    const actual = splitLongWord(longword, maxwidth, ctx).length;
-    const expected = 3;
-    assert.strictEqual(actual, expected);
+    const wordsNumber = splitLongWord(longword, maxwidth, ctx).length;
+    assert.strictEqual(wordsNumber, 3);
   });
-  it("flexRowWrap", () => {
-    const layout = new FlexRowWrapLayout(100, 5, 50, "center");
-    const blocks = [
-      { rect: new Rectangle(0, 0, 40, 40) },
-      { rect: new Rectangle(0, 0, 40, 40) },
-      { rect: new Rectangle(0, 0, 40, 40) },
+  it('MessageLayout.arrange', () => {
+    const layout = new MessageLayout(100, 5, 50);
+    const rects = [
+      new Rectangle(0, 0, 40, 40),
+      new Rectangle(0, 0, 40, 40),
+      new Rectangle(0, 0, 40, 40),
     ];
-    const actual = flexRowWrap(blocks, layout);
-    const expected = 100;
-    assert.strictEqual(actual, expected);
+    layout.arrange(rects);
+    const last = rects[rects.length - 1];
+    assert.strictEqual(last.y, 50);
   });
-  it("scrollDown", () => {
-    const blocks = [
-      { rect: new Rectangle(0, 0, 0, 10) },
-      { rect: new Rectangle(0, 10, 0, 10) },
-      { rect: new Rectangle(0, 20, 0, 10) },
+  it('scrollDown', () => {
+    const rects = [
+      new Rectangle(0, 0, 0, 10),
+      new Rectangle(0, 10, 0, 10),
+      new Rectangle(0, 20, 0, 10),
     ];
-    const bounds = new Rectangle(0, 10, 0, 10);
-    const actual = scrollDown(blocks, bounds);
-    console.log(blocks);
-    const expected = 2;
-    assert.strictEqual(actual, expected);
+    const minY = 10;
+    const maxY = 20;
+    const outOfBounds = scrollDown(rects, minY, maxY);
+    assert.strictEqual(outOfBounds, 2);
+  });
+  it('getDrawers', () => {
+    const message = 'hello world jopa';
+    const emotes = new Emotes(new Map([['world', null]]), 0);
+    const ctx = {
+      measureText() {
+        return {
+          width: 0,
+          actualBoundingBoxAscent: 0,
+        };
+      },
+    };
+    const drawers = getDrawers(message, emotes, Infinity, ctx);
+    assert.strictEqual(drawers.length, 3);
   });
 });
